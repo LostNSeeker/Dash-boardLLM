e76662280880cabc5e74652eacb542310be30e1a
Bipin <bk3284488@gmail.com>
2025-04-10 12:42:47 +0530

    Refactor firebaseConfig.js to dynamically set service account path based on environment

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,10 +1,17 @@
 var admin = require("firebase-admin");
 
+const serviceAccountPath =
+	process.env.NODE_ENV === "production"
+		? "/etc/secrets/serviceAccountKey.json"
+		: "./serverJson.json";
+
+console.log("Service Account Path:", serviceAccountPath);
+
 var serviceAccount = require("./serverJson.json");
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://dashboard-llm-default-rtdb.firebaseio.com"
+	databaseURL: "https://dashboard-llm-default-rtdb.firebaseio.com",
 });
 
 console.log("Firebase Admin SDK initialized successfully.");
